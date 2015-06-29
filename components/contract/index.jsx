1355676f56418c06eb61673716a9a2606e7c4c6c
Owen Yang <usay@outlook.com>
2015-06-29 22:06:58 +0800

    add correct text for contract

--- components/contract/index.jsx
+++ components/contract/index.jsx
@@ -1,13 +1,26 @@
 
 import React from 'react';
 
+const d = {
+  title: '美术作品著作权',
+  meta: '美术版权 | 06/24/2015',
+  excerpt: '企业的logo不仅仅是商标，更是重要版权；产品的外包装，做美术版权；企业全套VI设计更是需要版权的保护。',
+  extra: {
+    background: 'http://metaversemodsquad.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-26-at-10.36.15-AM.png'
+  }
+}
+
 const Contract = React.createClass({
   componentWillMount: function() {
   },
   render () {
+    const props = this.props;
+    const data = props.data;
+
     var styles = {
-      backgroundImage: 'url(http://metaversemodsquad.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-26-at-10.36.15-AM.png)'
+      backgroundImage: 'url(' + data.extra.background + ')'
     };
+    console.log(props);
 
     return (
       <div className="news-element">
@@ -15,9 +28,9 @@ const Contract = React.createClass({
           <a className="ne-img" style={styles} href="#"></a>
           <div className="ne-main">
               <div className="ne-copy">
-                  <div className="ne-headline">Metaverse Mod Squad Establishes European Operation Center in Northern Ireland</div>
-                  <div className="ne-meta">Metaverse | 06/24/2015</div>
-                  <div className="ne-excerpt">Brick and mortar alert!&nbsp; We are very excited to announce today the opening of our first European operation...</div>
+                  <div className="ne-headline">{data.title}</div>
+                  <div className="ne-meta">{data.meta}</div>
+                  <div className="ne-excerpt">{data.excerpt}</div>
               </div>
               <a className="ne-link" href="#"></a>
           </div>
--- src/channel/index.jsx
+++ src/channel/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 import Contract from '../../components/contract';
-const cinfos = [
-  1, 2, 3
-];
+
+const channel = [{
+  title: '美术作品著作权',
+  meta: '美术版权 | 06/24/2015',
+  excerpt: '企业的logo不仅仅是商标，更是重要版权；产品的外包装，做美术版权；企业全套VI设计更是需要版权的保护。',
+  extra: {
+    background: 'http://metaversemodsquad.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-26-at-10.36.15-AM.png'
+  }
+}, {
+  title: '文字作品著作权',
+  meta: '文字版权 | 03/01/2015',
+  excerpt: '每个企业都有自己的创业故事，把它保护起来吧！如果您是文创类的公司，人说IP为王，你的创作能产生巨大的商业价值；企业司歌的歌词，其实是可以作为文字版权的； 自媒体时代，随手的一段文字，都是你的版权。',
+  extra: {
+    background: 'http://metaversemodsquad.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-24-at-6.05.56-AM.png'
+  }
+}, {
+  title: '软件作品著作权',
+  meta: '计算机版权 | 01/10/2015',
+  excerpt: '新软件上线啦，要做计算机版权！！！软件迭代了，要做版权！！！企业做高新或其他一些科技项目，也需要软件著作权！！！',
+  extra: {
+    background: 'http://metaversemodsquad.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-18-at-12.48.36-PM.png'
+  }
+}]
+
 
 const PropTypes = React.PropTypes;
 
@@ -12,9 +33,9 @@ const ChannelPage = React.createClass({
   },
   render () {
     const props = this.props;
-    let contracts = cinfos.map(function(ctract, idx) {
+    let contracts = channel.map(function(ctract, idx) {
       return (
-        <Contract />
+        <Contract data={ctract} />
       );
     });
 
