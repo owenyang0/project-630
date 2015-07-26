0b16c3325522e58d8fd192ae7875644b34cb01bb
Owen Yang <usay@outlook.com>
2015-07-26 21:12:39 +0800

    footer | add footer style

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -6,17 +6,24 @@ const Footer = React.createClass({
   },
   render () {
     return (
-      <footer className="home-page__footer">
-        <div className="footer-head">
-          <h3>版权页示意</h3>
-        </div>
-        <div className="footer-legal">
-          <div className="footer-legal__state">
-            Imprint | 法律声明
+      <footer className="footer">
+        <div className='container-small'>
+          <div className='footer-content'>
+            <ul className='links'>
+              <li>
+                <a href="#">关于我们</a>
+              </li>
+            </ul>
+            <div className='qr'>
+              <img src="/assets/images/qr.png" alt="qr" />
+            </div>
           </div>
-          <div className="footer-legal__desc">
-            <p>Last Update: June 10, 2015</p>
-            <p>Copyright © 2015 Deutsche Bank AG, Frankfurt am Main</p>
+          <div className="footer-legal">
+            <div className="footer-legal__desc">
+              <p>Copyright © 2015 Deutsche Bank AG, All Rights Reserved. 京ICP备11015880号 | </p>
+            </div>
+            <a className="footer-legal__link" href='#'>用户留言</a>
+            <a className="footer-legal__link" href='#'>网站地图</a>
           </div>
         </div>
       </footer>
